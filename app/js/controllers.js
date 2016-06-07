0d83f5224d0d77c72aca7514e691182c49dab93d
jian_ <jian_yingshuo@msn.com>
2016-06-08 01:08:19 +0800

    update

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,22 @@
 'use strict'
 var app = angular.module("app");
-app.controller('ApplicationController', [function () {
+app.controller('ApplicationController', ['$scope', '$log', 'pageInfo', function ($scope, $log, pageInfo) {
+    $log.debug(pageInfo);
+
+    $scope.page = {
+        width: pageInfo.width + "px",
+        height: pageInfo.height + "px"
+    };
 
 }]);
 
 
-app.controller('homeCtrl', ['$scope', function ($scope) {
+app.controller('homeCtrl', ['$scope', 'pageInfo', function ($scope, pageInfo) {
     $scope.pageClass = 'pageHome';
+
+    $scope.background_img = {
+        width: pageInfo.width + "px"
+    };
+
     NProgress.done();
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,2 +1,17 @@
-'use strict'
-var app = angular.module("app");
+'use strict';
+angular.module("app")
+    .factory('pageInfo', [function () {
+        var pageWidth = $(window).width(), pageHeight = $(window).height(), pageRate = 1;
+        if (pageWidth < 640 || pageHeight < 1000) {
+            pageWidth = 640;
+            pageHeight = 1000;
+            pageRate = 1;
+        } else {
+            pageRate = pageWidth / 640;
+        }
+        return {
+            width: pageWidth,
+            height: pageHeight,
+            rate: pageRate
+        }
+    }]);
